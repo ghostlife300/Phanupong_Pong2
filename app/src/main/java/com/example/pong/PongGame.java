c51aa3b8eda80c2e06828c025e57d3cd24373a72
ghostlife300 <43547565+ghostlife300@users.noreply.github.com>
2021-01-10 18:41:42 +0700

    Add files via upload

--- app/src/main/java/com/example/pong/PongGame.java
+++ app/src/main/java/com/example/pong/PongGame.java
@@ -7,7 +7,8 @@ import android.graphics.Paint;
 import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
-
+import android.view.MotionEvent;
+import android.graphics.RectF;
 public class PongGame extends SurfaceView implements Runnable {
 
     private final boolean DEBUGGING = true;
@@ -35,23 +36,26 @@ public class PongGame extends SurfaceView implements Runnable {
         mScreenY = y;
         mFontSize = mScreenX / 20;
         mFontMargin = mScreenX / 75;
-
         mOurHolder = getHolder();
         mPaint = new Paint();
-
+        mBall = new Ball(mScreenX);
+        mBat = new Bat(mScreenX, mScreenY);
         startNewGame();
     }
     //พลังชีวิต
     private void startNewGame() {
         mScore = 0;
         mLives = 4;
+        mBall.reset(mScreenX, mScreenY);
     }
     private void draw() {
             if (mOurHolder.getSurface().isValid()) {
-                mCanvas = mOurHolder.lockCanvas(); // Lock the canvas (graphics memory)
+                mCanvas = mOurHolder.lockCanvas();
                 mCanvas.drawColor(Color.argb(255, 26, 128, 182));
                 mPaint.setColor(Color.argb(255, 255, 255, 255));
                 mPaint.setTextSize(mFontSize);
+                mCanvas.drawRect(mBall.getRect(), mPaint); // add code
+                mCanvas.drawRect(mBat.getRect(), mPaint);
                 mCanvas.drawText("Score: " + mScore + " Lives: " + mLives,
 
                         mFontMargin, mFontSize, mPaint);
@@ -89,16 +93,37 @@ public class PongGame extends SurfaceView implements Runnable {
     }
 
     private void update() {
-// Update the bat and the ball
+        mBall.update(mFPS);
+        mBat.update(mFPS);
     }
 
     private void detectCollisions(){
-// Has the bat hit the ball?
-// Has the ball hit the edge of the screen
-// Bottom
+        if(RectF.intersects(mBat.getRect(),
+                mBall.getRect())) {
+            mBall.batBounce(mBat.getRect());
+            mBall.increaseVelocity();
+            mScore++;
+        }
+        if(mBall.getRect().bottom > mScreenY){
+            mBall.reverseYVelocity();
+            mLives--;
+            if(mLives == 0){
+                mPaused = true;
+                startNewGame();
+            }
+        }
 // Top
+        if(mBall.getRect().top < 0){
+            mBall.reverseYVelocity();
+        }
 // Left
+        if(mBall.getRect().left < 0){
+            mBall.reverseXVelocity();
+        }
 // Right
+        if(mBall.getRect().right > mScreenX){
+            mBall.reverseXVelocity();
+        }
     }
 
     public void pause() {
@@ -120,6 +145,26 @@ public class PongGame extends SurfaceView implements Runnable {
         mGameThread = new Thread(this);
         mGameThread.start();
 
+    }
+    @Override
+    public boolean onTouchEvent(MotionEvent motionEvent) {
+        switch (motionEvent.getAction() &
+                MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_DOWN:
+                mPaused = false;
+                if (motionEvent.getX() > mScreenX / 2){
+                    mBat.setMovementState(mBat.RIGHT);
+                }
+                else
+                {
+                    mBat.setMovementState(mBat.LEFT);
+                }
+                break;
+                case MotionEvent.ACTION_UP:
+                mBat.setMovementState(mBat.STOPPED);
+                break;
+        }
+        return true;
     }
 
 }
